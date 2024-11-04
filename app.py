0bd7cf0c47aeae87c598eea3bd57cace496dac6f
Gádor <gador8@gmail.com>
2024-11-04 21:06:46 +0100

    pequeñas correcciones

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 import string
 import os
 
-load_dotenv('.env.dev')
+load_dotenv('.env.prod')
 
 app = Flask(__name__)
 
@@ -14,28 +14,32 @@ r = redis.Redis(host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"), decode_res
 
 CORS(app, resources={r'/*': {'origins': [os.getenv("FRONTEND_URL_DEV"), os.getenv("FRONTEND_URL"), os.getenv("FRONTEND_URL_PROD")]}})
 
+
 @app.route('/url', methods=['POST'])
 def create_url():
     characters = string.ascii_letters + string.digits
     url_original = request.json['urlOriginal']
 
     for key in r.scan_iter():
-      if r.get(key) == url_original:
+        if r.get(key) == url_original:
             return jsonify(key)
         
     url_acortada = ''.join(random.choice(characters) for i in range(15))
     r.set(url_acortada, url_original)
     return jsonify(url_acortada)
 
+
 @app.route('/urls', methods=['GET'])
 def get_urls():
-    urls  = list(map(lambda key: { key: r.get(key) }, r.keys()))
+    urls = list(map(lambda key: {key: r.get(key)}, r.keys()))
     return jsonify(urls)
+
 
 @app.route('/url/<url_acortada>', methods=['GET'])
 def get_url(url_acortada):
     url_original = r.get(url_acortada)
     return jsonify(url_original)
+
 
 @app.route('/url/<url_acortada>', methods=['DELETE'])
 def delete_url(url_acortada):
@@ -45,5 +49,6 @@ def delete_url(url_acortada):
     else:
         return 'Not found'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
