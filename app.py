c25b40a69bfee504582abdb41bb5f373cc1e7a05
Gádor <gador8@gmail.com>
2025-08-10 16:49:59 +0200

    Añadido Welcome y Keep alive

--- app.py
+++ app.py
@@ -12,43 +12,54 @@ app = Flask(__name__)
 
 r = redis.Redis(host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"), decode_responses=True)
 
-CORS(app, resources={r'/*': {'origins': [os.getenv("FRONTEND_URL_DEV"), os.getenv("FRONTEND_URL"), os.getenv("FRONTEND_URL_PROD")]}})
+CORS(app, resources={
+	r'/*': {'origins': [os.getenv("FRONTEND_URL_DEV"), os.getenv("FRONTEND_URL"), os.getenv("FRONTEND_URL_PROD")]}})
+
+
+@app.route('/')
+def welcome():
+	return jsonify('Bienvenidx a la API REST de URL Shortener'), 200
+
+
+@app.route('/keep-alive')
+def keep_alive():
+	return jsonify('Keep Alive URL Shortener realizado de forma satisfactoria'), 200
 
 
 @app.route('/url', methods=['POST'])
 def create_url():
-    characters = string.ascii_letters + string.digits
-    url_original = request.json['urlOriginal']
+	characters = string.ascii_letters + string.digits
+	url_original = request.json['urlOriginal']
 
-    for key in r.scan_iter():
-        if r.get(key) == url_original:
-            return jsonify(key)
-        
-    url_acortada = ''.join(random.choice(characters) for i in range(15))
-    r.set(url_acortada, url_original)
-    return jsonify(url_acortada)
+	for key in r.scan_iter():
+		if r.get(key) == url_original:
+			return jsonify(key)
+
+	url_acortada = ''.join(random.choice(characters) for i in range(15))
+	r.set(url_acortada, url_original)
+	return jsonify(url_acortada)
 
 
 @app.route('/urls', methods=['GET'])
 def get_urls():
-    urls = list(map(lambda key: {key: r.get(key)}, r.keys()))
-    return jsonify(urls)
+	urls = list(map(lambda key: {key: r.get(key)}, r.keys()))
+	return jsonify(urls)
 
 
 @app.route('/url/<url_acortada>', methods=['GET'])
 def get_url(url_acortada):
-    url_original = r.get(url_acortada)
-    return jsonify(url_original)
+	url_original = r.get(url_acortada)
+	return jsonify(url_original)
 
 
 @app.route('/url/<url_acortada>', methods=['DELETE'])
 def delete_url(url_acortada):
-    if r.exists(url_acortada):
-        r.delete(url_acortada)
-        return 'Success'
-    else:
-        return 'Not found'
+	if r.exists(url_acortada):
+		r.delete(url_acortada)
+		return 'Success'
+	else:
+		return 'Not found'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
